c35023bdabdc5ba9c57b760308ed4c27b7d408ea
Rebecca Tay <rebeccatay92@gmail.com>
2017-09-11 12:14:36 +0800

    flash msg for unique username fixed

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -5,9 +5,10 @@ function register (req, res) {
     name: req.body.user.name,
     password: req.body.user.password
   })
-  // create new user if username is unique
+  // create new user only if username is unique
   User.find({name: req.body.user.name}, function (err, docs) {
     if (docs.length) {
+      req.flash('msg', 'This username is already taken. Please choose another')
       return res.redirect('/users/register')
     } else {
       newUser.save(function (err, createdUser) {
@@ -15,7 +16,7 @@ function register (req, res) {
           req.flash('msg', err.message)
           return res.redirect('/users/register')
         }
-        res.redirect('/users/login') // ~~ client req GET /profile
+        res.redirect('/users/login')
       })
     }
   })
